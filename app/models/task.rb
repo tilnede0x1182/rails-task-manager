dd5be13e931088469c8c131a4005577db87cc7a2
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-02-25 16:38:30 +0100

    Ajout d'un bouton suppression dans le show

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :title, presence: { message: "Le titre est obligatoire !" }
+  validates :details, length: { minimum: 5, message: "Les détails doivent contenir au moins 5 caractères." }
 end
